fcfa6a836ee20f2088929f6966d5161b61445223
Tobias Brandner <tobias.brandner@stud-mail.uni-wuerzburg.de>
2024-04-18 14:55:57 +0200

    generate videos for testing

--- SRcode/data/generatevideo.py
+++ SRcode/data/generatevideo.py
@@ -3,34 +3,30 @@ import os
 from tqdm import tqdm
 
 
-def generate_videos(name: str) -> None:
-    path = f"../dataset/matrix/train"
-    save_path = f"../videos/{name}"
-
-    # generate_hr_video(path, name, save_path)
-
+def generate_videos(path: str, name: str, save_path: str) -> None:
+    generate_hr_video(path, name, save_path)
     generate_lr_videos(path, name, save_path)
 
 
 def generate_hr_video(path: str, name: str, save_path: str) -> None:
-    path += f"/HR/{name}"
-    name += "_hr"
-    generate_video(path, name, save_path)
+    path += "/HR"
+    generate_video(path, f"{name}", save_path)
 
 
 def generate_lr_videos(path: str, name, save_path: str) -> None:
-    path += f"/LR/{name}"
+    path += "/LR"
     sub_names = [#"basecolor", "metallic", "normal_vector", "roughness", "depth_10", "depth_log"
                  #"velocity", "velocity_log",
                     "world_normal", "world_position"]
-    # generate_video(path, f"{name}_lr", save_path)
+    generate_video(path, f"{name}", save_path)
 
-    for sub_name in sub_names:
-        generate_sub_video(path, f"{name}_{sub_name}", save_path, sub_name)
+    # for sub_name in sub_names:
+    #     generate_sub_video(path, f"{name}_{sub_name}", save_path, sub_name)
 
 
 def generate_video(path: str, name: str, save_path: str) -> None:
     # Get all PNG files in the directory
+    path = f"{path}/{name}"
     images = sorted([img for img in os.listdir(path) if img.endswith(".png") and img.count(".") == 1])
     frame = cv2.imread(os.path.join(path, images[0]))
     height, width, layers = frame.shape
@@ -65,11 +61,45 @@ def generate_sub_video(path: str, name: str, save_path: str, sub_type: str) -> N
     video.release()
 
 
-def main() -> None:
-    names = ["empire_state", "flat_iron", "ny_by_night", "ny_in_person"]
+def generate_network_videos() -> None:
+    path = "../results/test"
+    names = ["01", "02", "03", "04"]
     for name in names:
         print(f"Videos for {name}..")
-        generate_videos(name)
+        generate_video(path, name, "../videos")
+
+
+def generate_matrix_val_videos() -> None:
+    path = "../dataset/matrix/val_test"
+    names = ["01", "02", "03", "04"]
+    for name in names:
+        print(f"Generate video for {name}..")
+        # generate_hr_video(path, name, "../videos")
+
+        # 30 fps video
+        generate_lr_30_video(name, path, "../videos")
+
+
+def generate_lr_30_video(name, path, save_path: str) -> None:
+    path = f"{path}/LR/{name}"
+    images = sorted([img for img in os.listdir(path) if img.endswith(".png") and img.count(".") == 1])
+    frame = cv2.imread(os.path.join(path, images[0]))
+    height, width, layers = frame.shape
+    print(f"Height {height}, Width {width} and Layers {layers}")
+    # Define video codec and create VideoWriter object
+    fourcc = cv2.VideoWriter.fourcc(*'mp4v')  # Adjust codec as needed
+    video = cv2.VideoWriter(os.path.join(save_path, f"{name}.mp4"), fourcc, 30, (width, height))
+    counter = 0
+    for img in tqdm(images, desc=f"Generating video.."):
+        if counter % 2 == 0:
+            video.write(cv2.imread(os.path.join(path, img)))
+        counter += 1
+    cv2.destroyAllWindows()
+    video.release()
+
+
+def main() -> None:
+    generate_matrix_val_videos()
 
 
 if __name__ == "__main__":
