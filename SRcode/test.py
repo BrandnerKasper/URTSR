b578fbd63079a4fb3bce7ca9daaf74f8bb9d4069
Tobias Brandner <tobias.brandner@stud-mail.uni-wuerzburg.de>
2024-04-18 12:50:52 +0200

    test generates videos now for all val sequences

--- SRcode/test.py
+++ SRcode/test.py
@@ -1,17 +1,22 @@
-import yaml
+import os
 from torchvision import transforms
 import torch
 from tqdm import tqdm
-from torch.utils.data import DataLoader
 import torchvision.transforms.functional as F
 
-from data.dataloader import SingleImagePair, MultiImagePair
+from data.dataloader import SingleImagePair, MultiImagePair, DiskMode
 from SRcode.config import load_yaml_into_config, Config
 
 
 def get_config_from_pretrained_model(name: str) -> Config:
     config_path = f"configs/{name}.yaml"
     return load_yaml_into_config(config_path)
+
+
+def generate_directory(path):
+    # Create the directory if it doesn't exist
+    if not os.path.exists(path):
+        os.makedirs(path)
 
 
 def test() -> None:
@@ -32,18 +37,19 @@ def test() -> None:
 
     test_dataset = MultiImagePair(root=path, number_of_frames=4, last_frame_idx=599,
                          transform=transforms.ToTensor(), crop_size=None, scale=2,
-                         use_hflip=False, use_rotation=False, digits=4)
+                         use_hflip=False, use_rotation=False, digits=4, disk_mode=DiskMode.CV2)
 
     counter = 0
-    img_counter = 3 # we need history frames (number of frames -1)
     for idx in tqdm(range(len(test_dataset)), "Generating sequence.."):
         if counter % 2 == 1:
-            print("skipped\n")
+            # print("skipped\n")
             counter += 1
             continue
         counter += 1
         filename = test_dataset.get_filename(idx)
-        print(f"Filename: {filename}\n")
+        subfolder = test_dataset.get_path(idx).split("/")[0] # we want to retrieve the sub folder of the val sequences
+        generate_directory(f"{save_path}/{subfolder}")
+        # print(f"Filename: {filename}\n")
         lr_image, hr_image = test_dataset.__getitem__(idx)
         with torch.no_grad():
             lr_image = lr_image.unsqueeze(0)
@@ -52,10 +58,12 @@ def test() -> None:
             output_image = torch.clamp(output_image, min=0.0, max=1.0)
         # Safe generated images into a folder
         output_images = torch.unbind(output_image, 1)
-        for frame in output_images:
-            frame = F.to_pil_image(frame.squeeze(0))
-            frame.save(f"{save_path}/{img_counter:04d}.png")
-            img_counter += 1
+        for i in range(len(output_images)):
+            frame = F.to_pil_image(output_images[i].squeeze(0))
+            # generate the right filename
+            filename = int(filename) + i
+            # print(f"Save file at {save_path}/{subfolder}/{filename:04d}.png")
+            frame.save(f"{save_path}/{subfolder}/{filename:04d}.png")
 
 
 def main() -> None:
