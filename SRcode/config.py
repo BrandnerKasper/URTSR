e468083f9c6802cf6a0c0c86bf3f3acb9e776824
brandnerkasper <tobias.brandner@stud-mail.uni-wuerzburg.de>
2024-05-22 10:19:12 +0200

    mistake for testing reason

--- SRcode/config.py
+++ SRcode/config.py
@@ -162,9 +162,9 @@ def init_dataset(name: str, crop_size: int, use_hflip: bool, use_rotation: bool)
         #                          use_hflip=use_hflip, use_rotation=use_rotation, digits=4, disk_mode=DiskMode.CV2)
         #     return train, val
         case "ue_data":
-            train = STSSImagePair(root=f"{root}/train", scale=2, history=3, last_frame_idx=10, crop_size=crop_size,
+            train = STSSImagePair(root=f"{root}/train", scale=2, history=3, last_frame_idx=299, crop_size=crop_size,
                                   use_hflip=use_hflip, use_rotation=use_rotation, digits=4)
-            val = STSSImagePair(root=f"{root}/val", scale=2, history=3, last_frame_idx=10, crop_size=crop_size,
+            val = STSSImagePair(root=f"{root}/val", scale=2, history=3, last_frame_idx=299, crop_size=crop_size,
                                 use_hflip=use_hflip, use_rotation=use_rotation, digits=4)
             return train, val
         # case "ue_data_npz":
