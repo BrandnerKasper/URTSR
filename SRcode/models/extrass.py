b4ac14a028333c1308162712aa731b5c403a1642
brandnerkasper <tobias.brandner@stud-mail.uni-wuerzburg.de>
2024-05-29 18:30:08 +0200

    first version of extrass

--- SRcode/models/extrass.py
+++ SRcode/models/extrass.py
@@ -46,70 +46,134 @@ def depth_to_space(input_tensor, block_size):
     return output_tensor
 
 
+class FRNet(nn.Module):
+    def __init__(self):
+        super(FRNet, self).__init__()
+
+        # Define the down-sampling blocks
+        self.down1 = self.conv_block(3*3, 22)
+        self.down2 = self.conv_block(22, 24)
+        self.down3 = self.conv_block(24, 36)
+        self.down4 = self.conv_block(36, 48)
+
+        # Define the up-sampling blocks
+        self.up1 = self.up_conv_block(48, 36)
+        self.up2 = self.up_conv_block(36, 24)
+        self.up3 = self.up_conv_block(24, 22)
+        self.up4 = self.up_conv_block(22, 5)
+
+        # Define the blending layer
+        self.blend = nn.Sequential(
+            nn.Conv2d(48, 5, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True)
+        )
+
+    def conv_block(self, in_channels, out_channels):
+        return nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(inplace=True)
+        )
+
+    def up_conv_block(self, in_channels, out_channels):
+        return nn.Sequential(
+            nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        # Down-sampling path
+        x1 = self.down1(x)
+        x2 = self.down2(x1)
+        x3 = self.down3(x2)
+        x4 = self.down4(x3)
+
+        # Blending the down-sampled features
+        # blend_input = torch.cat((x4, x2_d4), dim=1)
+        # x = self.blend(x4)
+
+        # Up-sampling path
+        x = self.up1(x4)
+        x = self.up2(x + x3)
+        x = self.up3(x + x2)
+        x = self.up4(x + x1)
+
+        return x
+
+
 class ExtraSS(BaseModel):
     def __init__(self, scale: int):
-        super(ExtraSS, self).__init__(scale=scale)
+        super(ExtraSS, self).__init__(scale=scale, down_and_up=4)
 
-        self.hr_input = torch.randn(1, 3, 3840, 2160) # abstract batch and crop size for this to work
+        self.hr_input = torch.randn(1, 3, 3840, 2176).to(device='cuda') # abstract batch and crop size for this to work
+        self.extra = False
+        self.fr = FRNet()
 
-        self.down_1_e0 = nn.Sequential(
-            nn.Conv2d(3, 22, kernel_size=3, stride=1, padding=1),
+        # Encoder for the SS forward pass
+        self.ss_red_1 = nn.Conv2d(3 + 9, 22, kernel_size=3, stride=1, padding=1)
+        self.ss_down_1 = nn.Sequential(
+            nn.Conv2d(22, 22, kernel_size=3, stride=1, padding=1),
             nn.ReLU(inplace=True),
             nn.Conv2d(22, 22, kernel_size=3, stride=2, padding=1)
         )
-        self.down_2_e0 = nn.Sequential(
-            nn.Conv2d(22, 32, kernel_size=3, stride=1, padding=1),
+        self.ss_red_2 = nn.Conv2d(22, 32, kernel_size=3, stride=1, padding=1)
+        self.ss_down_2 = nn.Sequential(
+            nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),
             nn.ReLU(inplace=True),
             nn.Conv2d(32, 32, kernel_size=3, stride=2, padding=1)
         )
-        self.down_3_e0 = nn.Sequential(
-            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+        self.ss_red_3 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.ss_down_3 = nn.Sequential(
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
             nn.ReLU(inplace=True),
             nn.Conv2d(64, 64, kernel_size=3, stride=2, padding=1)
         )
-        self.down_4_e0 = nn.Sequential(
-            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+        self.ss_red_4 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.ss_down_4 = nn.Sequential(
+            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
             nn.ReLU(inplace=True),
-            nn.Conv2d(128, 128, kernel_size=3, stride=2, padding=1)
+            nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1)
         )
-        self.down_1_e1 = nn.Sequential(
-            nn.Conv2d(3 + 12, 22, kernel_size=3, stride=1, padding=1),
+        self.ss_red_5 = nn.Sequential(
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True)
+        )
+
+        # Encoder for the ESS forward pass
+        self.ess_red_1 = nn.Conv2d(3 + 9 + 12 + 5, 22, kernel_size=3, stride=1, padding=1)
+        self.ess_down_1 = nn.Sequential(
+            nn.Conv2d(22, 22, kernel_size=3, stride=1, padding=1),
             nn.ReLU(inplace=True),
             nn.Conv2d(22, 22, kernel_size=3, stride=2, padding=1)
         )
-        self.down_2_e1 = nn.Sequential(
-            nn.Conv2d(22, 32, kernel_size=3, stride=1, padding=1),
+        self.ess_red_2 = nn.Conv2d(22, 32, kernel_size=3, stride=1, padding=1)
+        self.ess_down_2 = nn.Sequential(
+            nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),
             nn.ReLU(inplace=True),
             nn.Conv2d(32, 32, kernel_size=3, stride=2, padding=1)
         )
-        self.down_3_e1 = nn.Sequential(
-            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+        self.ess_red_3 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.ess_down_3 = nn.Sequential(
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
             nn.ReLU(inplace=True),
             nn.Conv2d(64, 64, kernel_size=3, stride=2, padding=1)
         )
-        self.down_4_e1 = nn.Sequential(
-            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+        self.ess_red_4 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.ess_down_4 = nn.Sequential(
+            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
             nn.ReLU(inplace=True),
-            nn.Conv2d(128, 128, kernel_size=3, stride=2, padding=1)
+            nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1)
+        )
+        self.ess_red_5 = nn.Sequential(
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True)
         )
 
-        # two different encoders for the ss and ess forward pass
-        # self.encoder_0 = nn.Sequential(
-        #     self.down_1_e0,
-        #     self.down_2_e0,
-        #     self.down_3_e0,
-        #     self.down_4_e0
-        # )
-        # self.encoder_1 = nn.Sequential(
-        #     self.down_1_e1,
-        #     self.down_2_e1,
-        #     self.down_3_e1,
-        #     self.down_4_e1
-        # )
-
+        # Shared Decoder
         self.up_1 = nn.Sequential(
-            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(inplace=True),
             nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
         )
         self.up_2 = nn.Sequential(
@@ -133,11 +197,55 @@ class ExtraSS(BaseModel):
             nn.ReLU(inplace=True),
         )
         self.finish_2 = nn.Sequential(
-            nn.Conv2d(58, 12, kernel_size=3, stride=1, padding=1)
+            nn.Conv2d(68, 12, kernel_size=3, stride=1, padding=1)
         )
 
-    def forward(self, x):
-        pass
+    def forward(self, x: torch.Tensor, features: torch.Tensor, his: torch.Tensor):
+        x = torch.cat([x, features], dim=1)
+        # SS Encoder forward pass
+        if not self.extra:
+            x1 = self.ss_red_1(x)
+            x2 = self.ss_down_1(x1)
+            x2 = self.ss_red_2(x2)
+            x3 = self.ss_down_2(x2)
+            x3 = self.ss_red_3(x3)
+            x4 = self.ss_down_3(x3)
+            x4 = self.ss_red_4(x4)
+            x5 = self.ss_down_4(x4)
+            x5 = self.ss_red_5(x5)
+        # ESS Encoder forward pass
+        else:
+            # Use SS frame as additonal input
+            hr = space_to_depth(self.hr_input, 2)
+            # Use FRNet as additional input
+            his = torch.cat(torch.unbind(his, 1), 1)
+            his = self.fr(his)
+            x = torch.cat([x, hr, his], dim=1)
+            x1 = self.ess_red_1(x)
+            x2 = self.ess_down_1(x1)
+            x2 = self.ess_red_2(x2)
+            x3 = self.ess_down_2(x2)
+            x3 = self.ess_red_3(x3)
+            x4 = self.ess_down_3(x3)
+            x4 = self.ess_red_4(x4)
+            x5 = self.ess_down_4(x4)
+            x5 = self.ess_red_5(x5)
+        # Shared Decoder
+        x = self.up_1(x5)
+        x = self.up_2(x+x4)
+        x = self.up_3(x+x3)
+        x = self.up_4(x+x2)
+        x = self.finish_1(x+x1)
+        x = torch.cat([x, x1], dim=1)
+        x = self.finish_2(x)
+        x = depth_to_space(x, 2)
+        # Safe the SS output as input for the ESS pass
+        if not self.extra:
+            self.hr_input = x
+            self.extra = True
+        else:
+            self.extra = False
+        return x
 
 
 def main() -> None:
@@ -145,9 +253,9 @@ def main() -> None:
 
     model = ExtraSS(scale=2).to(device)
     batch_size = 1
-    input_data = (batch_size, 3, 1920, 1080)
-    feature = (batch_size, 9, 1920, 1080)
-    his = (batch_size, 3, 2, 1920, 1080)
+    input_data = (batch_size, 3, 1920, 1088)
+    feature = (batch_size, 9, 1920, 1088)
+    his = (batch_size, 3, 3, 1920, 1088)
     input_size = (input_data, feature, his)
 
     model.summary(input_size)
