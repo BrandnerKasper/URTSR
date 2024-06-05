59ce41e551c40f54ec5d06a88377486fa748d35f
brandnerkasper <tobias.brandner@stud-mail.uni-wuerzburg.de>
2024-06-05 18:10:43 +0200

    updated generatevideo.py

--- SRcode/data/generatevideo.py
+++ SRcode/data/generatevideo.py
@@ -3,28 +3,13 @@ import os
 from tqdm import tqdm
 
 
-def generate_videos(path: str, name: str, save_path: str) -> None:
-    generate_hr_video(path, name, save_path)
-    generate_lr_videos(path, name, save_path)
+def generate_directory(path):
+    # Create the directory if it doesn't exist
+    if not os.path.exists(path):
+        os.makedirs(path)
 
 
-def generate_hr_video(path: str, name: str, save_path: str) -> None:
-    path += "/HR"
-    generate_video(path, f"{name}", save_path)
-
-
-def generate_lr_videos(path: str, name, save_path: str) -> None:
-    path += "/LR"
-    sub_names = [#"basecolor", "metallic", "normal_vector", "roughness", "depth_10", "depth_log"
-                 #"velocity", "velocity_log",
-                    "world_normal", "world_position"]
-    generate_video(path, f"{name}", save_path)
-
-    # for sub_name in sub_names:
-    #     generate_sub_video(path, f"{name}_{sub_name}", save_path, sub_name)
-
-
-def generate_video(path: str, name: str, save_path: str) -> None:
+def generate_video(path: str, name: str, save_path: str, lr: bool = False) -> None:
     # Get all PNG files in the directory
     path = f"{path}/{name}"
     images = sorted([img for img in os.listdir(path) if img.endswith(".png") and img.count(".") == 1])
@@ -34,28 +19,22 @@ def generate_video(path: str, name: str, save_path: str) -> None:
 
     # Define video codec and create VideoWriter object
     fourcc = cv2.VideoWriter.fourcc(*'mp4v')  # Adjust codec as needed
-    video = cv2.VideoWriter(os.path.join(save_path, f"{name}.mp4"), fourcc, 60, (width, height))
+    if lr:
+        video = cv2.VideoWriter(os.path.join(save_path, f"{name}.mp4"), fourcc, 30, (width, height))
+    else:
+        video = cv2.VideoWriter(os.path.join(save_path, f"{name}.mp4"), fourcc, 60, (width, height))
 
-    for img in tqdm(images, desc=f"Generating video.."):
-        video.write(cv2.imread(os.path.join(path, img)))
-
-    cv2.destroyAllWindows()
-    video.release()
-
-
-def generate_sub_video(path: str, name: str, save_path: str, sub_type: str) -> None:
-    # Get all PNG files in the directory
-    images = sorted([img for img in os.listdir(path) if img.endswith(f"{sub_type}.png")])
-    frame = cv2.imread(os.path.join(path, images[0]))
-    height, width, layers = frame.shape
-    print(f"Height {height}, Width {width} and Layers {layers}")
-
-    # Define video codec and create VideoWriter object
-    fourcc = cv2.VideoWriter.fourcc(*'mp4v')  # Adjust codec as needed
-    video = cv2.VideoWriter(os.path.join(save_path, f"{name}.mp4"), fourcc, 60, (width, height))
-
-    for img in tqdm(images, desc=f"Generating sub video {sub_type}.."):
-        video.write(cv2.imread(os.path.join(path, img)))
+    if lr:
+        counter = 0
+        for img in tqdm(images, desc=f"Generating video.."):
+            if counter % 2 == 1:
+                counter += 1
+                continue
+            counter += 1
+            video.write(cv2.imread(os.path.join(path, img)))
+    else:
+        for img in tqdm(images, desc=f"Generating video.."):
+            video.write(cv2.imread(os.path.join(path, img)))
 
     cv2.destroyAllWindows()
     video.release()
@@ -70,45 +49,15 @@ def generate_network_videos() -> None:
         generate_video(path, name, f"../videos/extraSS_All")
 
 
-def generate_train_videos() -> None:
-    path = "../dataset/ue_data/train"
-    names = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
-    for name in names:
-        print(f"Generate video for {name}..")
-        generate_hr_video(path, name, f"../videos/HR/{name}")
-
-        # 30 fps video
-        generate_lr_30_video(name, path, f"../videos/LR/{name}")
-
-
-def generate_lr_30_video(name, path, save_path: str) -> None:
-    path = f"{path}/LR/{name}"
-    images = sorted([img for img in os.listdir(path) if img.endswith(".png") and img.count(".") == 1])
-    frame = cv2.imread(os.path.join(path, images[0]))
-    height, width, layers = frame.shape
-    print(f"Height {height}, Width {width} and Layers {layers}")
-    # Define video codec and create VideoWriter object
-    fourcc = cv2.VideoWriter.fourcc(*'mp4v')  # Adjust codec as needed
-    generate_directory(save_path)
-    video = cv2.VideoWriter(os.path.join(save_path, f"{name}.mp4"), fourcc, 30, (width, height))
-    counter = 0
-    for img in tqdm(images, desc=f"Generating video.."):
-        if counter % 2 == 0:
-            video.write(cv2.imread(os.path.join(path, img)))
-        counter += 1
-    cv2.destroyAllWindows()
-    video.release()
-
-
-def generate_directory(path):
-    # Create the directory if it doesn't exist
-    if not os.path.exists(path):
-        os.makedirs(path)
+def generate_videos(file_path: str, save_name: str) -> None:
+    generate_directory(f"../videos/{save_name}")
+    for directory in os.listdir(file_path):
+        print(f"Generate video for {directory}..")
+        generate_video(file_path, directory, save_name)
 
 
 def main() -> None:
-    # generate_train_videos()
-    generate_network_videos()
+    generate_videos("../results/extraSS_All", "test")
 
 
 if __name__ == "__main__":
